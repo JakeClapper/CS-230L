c04ab8b1f2e9867986865eaca3ddb2be1dd4d591
JakeClapper <88346685+JakeClapper@users.noreply.github.com>
2023-11-01 16:49:55 -0400

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import Navbar1 from './Navbar';
+import Card from './Card';
+
 function App() {
   return (
     <div className="App">
-      <h1>CS 230L</h1>
-      <h2>Section -003</h2>
-      <p>WVU ID: 800387090</p>
-      <p> Hi Im Jake</p>
-      
-      
+      <Navbar />
+      <div className="card-container">
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Card() {
+  return (
+    <div className="card">
+      <h3>Card Title</h3>
+      <p>Card content goes here.</p>
+    </div>
+  );
+}
+
+export default Card;

0d6849515ae40217a3be0e2e4c1c6f8f065fad28
JakeClapper <88346685+JakeClapper@users.noreply.github.com>
2023-11-02 16:56:33 -0400

    yghrtggtfwfgt

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React from "react";
 
-function Card() {
+const Card = () => {
   return (
-    <div className="card">
-      <h3>Card Title</h3>
-      <p>Card content goes here.</p>
-    </div>
+    <>
+      <div className="d-flex justify-content-around text-center w-400">
+        <div
+          className="card text-white bg-primary mb-3 text-center custom-width"
+           styles={{maxWidth: '18rem'}}
+        >
+          <div className="card-header">
+            <h4>Card 01</h4>
+          </div>
+          <div className="card-body">
+            <p className="card-text">
+              Some quixk example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+        <div className="card text-white bg-danger mb-3" style="max-width: 18rem">
+          <div className="card-header">
+            <h4>Card 03</h4>
+          </div>
+          <div className="card-body">
+            <p className="card-text">
+              Some quixk example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+        <div className="card text-white bg-success mb-3" style="max-width: 18rem">
+          <div className="card-header">
+            <h4>Card 02</h4>
+          </div>
+          <div className="card-body">
+            <p className="card-text">
+              Some quixk example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default Card;
